8d18be33270bf6e53658f8444025e29922dae6f8
zhouyi <dugu9sword@163.com>
2019-11-20 02:18:53 +0800

    deprecate support for contiguous embeddings

--- dpattack/cmds/zhou/hackwhole.py
+++ dpattack/cmds/zhou/hackwhole.py
@@ -19,6 +19,19 @@ from dpattack.libs.luna import log_config, log, fetch_best_ckpt_name, show_mean_
     ram_write, ram_read, TrainingStopObserver, CherryPicker, Aggregator, time, time_stamp
 from dpattack.utils.vocab import Vocab
 from dpattack.libs.luna import create_folder_for_file
+from dpattack.libs.luna.public import auto_create
+from dpattack.utils.tag_tool import gen_tag_dict
+
+# Code for fucking VSCode debug console
+
+
+class V:
+    def __sub__(self, tsr):
+        for ele in tsr.__repr__().split('\n'):
+            print(ele)
+
+
+v = V()
 
 
 class _Tags:
@@ -42,32 +55,18 @@ class _Tags:
 hack_tags = _Tags()
 
 
-def mask_to_small(val):
-    if val > 0:
-        val = -val
-    val = val - 1000
-    return val
+# def generate_tag_filter(corpus, vocab):
+#     tag_filter = defaultdict(lambda: set())
+#     train_words = vocab.n_train_words
 
-
-def mask_to_large(val):
-    if val < 0:
-        val = - val
-    val = val + 1000
-    return val
-
-
-def generate_tag_filter(corpus, vocab):
-    tag_filter = defaultdict(lambda: set())
-    train_words = vocab.n_train_words
-
-    for word_seq, tag_seq in zip(corpus.words, corpus.tags):
-        for word, tag in zip(word_seq[1:], tag_seq[1:]):
-            word_id = vocab.word_dict.get(word.lower(), vocab.unk_index)
-            if word_id != vocab.unk_index and word_id < train_words:
-                tag_filter[tag].add(word)
-    for key, value in tag_filter.items():
-        tag_filter[key] = vocab.word2id(list(value))
-    return tag_filter
+#     for word_seq, tag_seq in zip(corpus.words, corpus.tags):
+#         for word, tag in zip(word_seq[1:], tag_seq[1:]):
+#             word_id = vocab.word_dict.get(word.lower(), vocab.unk_index)
+#             if word_id != vocab.unk_index and word_id < train_words:
+#                 tag_filter[tag].add(word)
+#     for key, value in tag_filter.items():
+#         tag_filter[key] = vocab.word2id(list(value))
+#     return tag_filter
 
 
 class HackWhole:
@@ -78,9 +77,6 @@ class HackWhole:
         self.embed_searcher: EmbeddingSearcher
         self.tagger: PosTagger
 
-        self.contiguous_embed = {}
-        self.__contiguous_embed_flag = None
-
     @property
     def vocab(self) -> Vocab:
         return self.task.vocab
@@ -89,25 +85,16 @@ class HackWhole:
     def parser(self) -> Union[WordTagParser, WordParser]:
         return self.task.model
 
-    @contextmanager
-    def use_contiguous_embed(self, flag=True):
-        # whatever the flag, after execution it will be set to False
-        if self.__contiguous_embed_flag is not None:
-            raise Exception('Do not use a use_contiguous_embed '
-                            'within a use_contiguous_embed')
-        self.__contiguous_embed_flag = flag
-        yield
-        self.__contiguous_embed_flag = None
-
     def __call__(self, config):
-        log_config('hackwhole-{}-{}-{}'.format(config.hk_loss_based_on,
-                                               config.hk_step_size,
-                                               config.hk_pgd_freq),
-                   log_path=config.workspace,
-                   default_target='cf')
-        attack_output_path = config.hk_output_path + time_stamp()
-        create_folder_for_file(attack_output_path)
-        fout = open(attack_output_path, "w")
+        if config.logf == 'on':
+            log_config('hackwhole-{}-{}-{}'.format(config.hk_loss_based_on,
+                                                   config.hk_step_size,
+                                                   config.hk_pgd_freq),
+                       log_path=config.workspace,
+                       default_target='cf')
+        else:
+            log = print
+
         for arg in config.kwargs:
             if arg.startswith('hk'):
                 log(arg, '\t', config.kwargs[arg])
@@ -123,27 +110,19 @@ class HackWhole:
         print("Load the dataset")
 
         train_corpus = Corpus.load(config.ftrain)
-        self.tag_filter = generate_tag_filter(train_corpus, vocab)
+        # self.tag_dict = generate_tag_filter(train_corpus, vocab)
+        self.tag_dict = auto_create("tagdict3", lambda: gen_tag_dict(train_corpus, vocab, 3, False),
+                                    cache=True, path=config.workspace+'/saved_vars')
+        self.tag_dict = {k: torch.tensor(v) for k,v in self.tag_dict.items()}
         corpus = Corpus.load(config.fdata)
         dataset = TextDataset(vocab.numericalize(corpus, True))
         # set the data loader
         loader = DataLoader(dataset=dataset,
                             collate_fn=collate_fn)
 
-        def embed_forward_hook(module, input, output):
-            # output_bak = output.clone()
-            # print('before: ', output[0, :, :3])
-            if self.__contiguous_embed_flag:
-                # print('USE CONTIGUOUS')
-                for idx, val in self.contiguous_embed.items():
-                    output[0][idx] = val
-            # print('after: ', output[0, :, :3])
-            # print(output - output_bak)
-
         def embed_backward_hook(module, grad_in, grad_out):
             ram_write('embed_grad', grad_out[0])
 
-        parser.embed.register_forward_hook(embed_forward_hook)
         parser.embed.register_backward_hook(embed_backward_hook)
 
         self.embed_searcher = EmbeddingSearcher(
@@ -172,7 +151,6 @@ class HackWhole:
                 [(raw_words, tags, None, arcs, rels)])
 
             self.forbidden_idxs = [vocab.unk_index, vocab.pad_index]
-            self.contiguous_embed = {}
             self.change_positions = set()
             log('****** {}: \n{}\n{}'.format(sid,
                                              " ".join(words_text), " ".join(tags_text)))
@@ -184,11 +162,6 @@ class HackWhole:
             t0 = time.time()
             # HIGHLIGHT: ITERATION
             for iter_id in range(1, config.hk_steps):
-
-                cgs_flg = iter_id % config.hk_pgd_freq != 0
-                if not cgs_flg:
-                    self.contiguous_embed = {}
-
                 if isinstance(config.hk_max_change, int):
                     max_change_num = config.hk_max_change
                 elif isinstance(config.hk_max_change, float):
@@ -203,11 +176,11 @@ class HackWhole:
                     raw_words=raw_words, raw_metric=raw_metric,
                     dist_measure=config.hk_dist_measure,
                     loss_based_on=config.hk_loss_based_on,
-                    cgs_flg=cgs_flg,
                     step_size=config.hk_step_size,
                     verbose=False,
                     max_change_num=max_change_num,
-                    iter_id=iter_id
+                    iter_id=iter_id,
+                    repl_method=config.hk_repl_method
                 )
                 # Fail
                 if result['code'] == 404:
@@ -221,6 +194,7 @@ class HackWhole:
                         log('Succeed in step {}'.format(iter_id))
                         break
                 var_words = result['words']
+            v - picker.history_infos
             t1 = time.time()
             raw_metrics += raw_metric
             best_iter, best_attack_metric, best_info = picker.select_best_point()
@@ -231,14 +205,6 @@ class HackWhole:
             log('Show result from iter {}, max change {}:'.format(
                 best_iter, max_change_num))
             log(best_info)
-            for i in range(1, var_words.size(1)):
-                fout.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
-                    i, vocab.words[var_words[0][i]], '_',
-                    vocab.tags[tags[0][i]], vocab.tags[tags[0][i]],
-                    '_', arcs[0][i].item(), vocab.rels[rels[0][i]], '_', '_'
-                ))
-            fout.write('\n')
-            fout.flush()
             log('Aggregated result: {} --> {}, '
                 'iters(avg) {:.1f}, time(avg) {:.1f}s, '
                 'fail rate {:.2f}, best_iter(avg) {:.1f}, best_iter(std) {:.1f}, '
@@ -252,13 +218,11 @@ class HackWhole:
             log()
 
     def backward_loss(self, words, tags, arcs, rels,
-                      cgs_flg,
                       loss_based_on) -> torch.Tensor:
         self.parser.zero_grad()
         mask = words.ne(self.vocab.pad_index)
         mask[:, 0] = 0
-        with self.use_contiguous_embed(cgs_flg):
-            s_arc, s_rel = self.parser(words, tags)
+        s_arc, s_rel = self.parser(words, tags)
         s_arc, s_rel = s_arc[mask], s_rel[mask]
         gold_arcs, gold_rels = arcs[mask], rels[mask]  # shape like [7,7,7,0,3]
 
@@ -271,8 +235,7 @@ class HackWhole:
             margin = s_gold - max_s_other
             margin[margin < -1] = -1
             loss = margin.sum()
-            # with self.use_contiguous_embed(cgs_flg):
-            #     current_embed = parser.embed(words)[0]
+            # current_embed = parser.embed(words)[0]
             # raw_embed = parser.embed(raw_words)
             # change_penalty = torch.norm(current_embed - raw_embed,
             #                             p=2, dim=1).sum()
@@ -332,14 +295,14 @@ class HackWhole:
             # self.embed_searcher.find_neighbours(changed, 10, 'euc', True)
 
             # Mask illegal words by its POS
-            legal_tag_index = self.tag_filter[must_tag].to(dist.device)
+            legal_tag_index = self.tag_dict[must_tag].to(dist.device)
             legal_tag_mask = dist.new_zeros(dist.size()) \
                 .index_fill_(0, legal_tag_index, 1.).byte()
             dist.masked_fill_(1 - legal_tag_mask, 1000.)
             for ele in self.forbidden_idxs:
                 dist[ele] = 1000.
             new_word_vid = dist.argmin()
-            return new_word_vid, {}
+            return new_word_vid, {"avgd": 0, "mind": 0}
         elif repl_method == 'bert':
             # Pipeline:
             #    Bert select 256 words
@@ -354,17 +317,16 @@ class HackWhole:
                     dist_measure='euc',
                     loss_based_on='logit',
                     step_size=5,
-                    cgs_flg=False,
                     verbose=False,
                     max_change_num=1,
-                    iter_id=-1):
+                    iter_id=-1,
+                    repl_method="tagger"):
         sent_len = words.size(1)
 
         """
             Loss back-propagation
         """
         embed_grad = self.backward_loss(words, tags, arcs, rels,
-                                        cgs_flg=cgs_flg,
                                         loss_based_on=loss_based_on)
         # Sometimes the loss/grad will be zero.
         # Especially in the case of applying pgd_freq>1 to small sentences:
@@ -400,10 +362,7 @@ class HackWhole:
         max_grad = embed_grad[0][word_sid]
 
         word_vid = words[0][word_sid]
-        if cgs_flg and word_sid.item() in self.contiguous_embed:
-            emb_to_rpl = self.contiguous_embed[word_sid.item()]
-        else:
-            emb_to_rpl = self.parser.embed.weight[word_vid]
+        emb_to_rpl = self.parser.embed.weight[word_vid]
         self.forbidden_idxs.append(word_vid.item())
         self.change_positions.add(word_sid.item())
         # print(self.change_positions)
@@ -415,7 +374,8 @@ class HackWhole:
         must_tag = self.vocab.tags[tags[0][word_sid].item()]
         new_word_vid, repl_info = self.find_replacement(words, word_sid,
                                                         changed, must_tag,
-                                                        dist_measure, 'tagger')
+                                                        dist_measure,
+                                                        repl_method)
 
         # A forbidden word maybe chosen if all words are forbidden(1000.)
         if new_word_vid is None or new_word_vid.item() in self.forbidden_idxs:
@@ -423,9 +383,6 @@ class HackWhole:
             return {'code': 404,
                     'info': "Selected word are forbidden"}
 
-        # For contiguous version, the new_word is the projected version.
-        if cgs_flg:
-            self.contiguous_embed[word_sid.item()] = changed
         new_words = words.clone()
         new_words[0][word_sid] = new_word_vid
 
@@ -434,6 +391,8 @@ class HackWhole:
         """
         # print('START EVALUATING')
         # print([self.vocab.words[ele] for ele in self.forbidden_idxs])
+        new_words_text = [self.vocab.words[i.item()] for i in new_words[0]]
+        # print(new_words_txt)
         loss, metric = self.task.evaluate(
             [(new_words, tags, None, arcs, rels)])
 
@@ -451,7 +410,8 @@ class HackWhole:
                 table.append([
                     i,
                     raw_words_text[i],
-                    '>{}'.format(new_words_text[i]) if raw_words_text[i] != new_words_text[i] else "*",
+                    '>{}'.format(
+                        new_words_text[i]) if raw_words_text[i] != new_words_text[i] else "*",
                     tags_text[i],
                     gold_arc,
                     '>{}'.format(pred_arc) if pred_arc != gold_arc else '*',
@@ -461,7 +421,7 @@ class HackWhole:
 
         if verbose:
             print('$$$$$$$$$$$$$$$$$$$$$$$$$$$')
-            print('Iter {}, cgs_flg={}'.format(iter_id, cgs_flg))
+            print('Iter {}'.format(iter_id))
             # print('After Attacking: \n\t{}\n\t{}'.format(
             #     " ".join(new_words_text), " ".join(tags_text)))
             # print('{} --> {}'.format(
@@ -471,8 +431,8 @@ class HackWhole:
             print(tabulate(_gen_log_table(), floatfmt=('.6f')))
             print('^^^^^^^^^^^^^^^^^^^^^^^^^^^')
 
-        log('iter {}, pgd {}, uas {:.4f}, mind {:6.3f}, avgd {:6.3f}'.format(
-            iter_id, int(not cgs_flg), metric.uas, repl_info['mind'], repl_info['avgd']))
+        log('iter {}, uas {:.4f}, mind {:6.3f}, avgd {:6.3f}'.format(
+            iter_id, metric.uas, repl_info['mind'], repl_info['avgd']))
         if metric.uas >= raw_metric.uas - .00001:
             info = 'Nothing'
         else:
